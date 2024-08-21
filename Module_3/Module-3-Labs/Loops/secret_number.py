d0fe8d115f3d753e61c6634bd96ff39febbfe40d
Joseph Clay <j.clay.2064@gmail.com>
2024-08-20 21:02:52 -0500

    Testing 2

--- Module_3/Module-3-Labs/Loops/secret_number.py
+++ Module_3/Module-3-Labs/Loops/secret_number.py
@@ -1,4 +1,21 @@
-# Guessing game to guess the secret number
+import random
+ 
 
+randomInteger = random.randint(1, 100)
 
-secret_number = # random number
+print ("Welcome to guess the secret number game ")
+print (" I have guessed a secret number between 1 and 100")
+
+# Start the game
+while True:
+     # Ask the user to enter a Integer number
+     userGuessNumber = int(input("Enter an integer number"))
+     
+     # Checked if guessed number is correct 
+     if userGuessNumber == randomInteger:
+           print ("The number was", randomInteger)
+           print ("Well done, muggle! You are free now!")
+           break # Exit the loop
+     else:
+           # If incorrect, print the message and prompt again
+           print ("Ha ha, You're stuck in my loop")
