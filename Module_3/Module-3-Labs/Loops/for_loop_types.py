f6c72223e318033b9e80f19304bbcc18bc985cd1
Joseph Clay <j.clay.2064@gmail.com>
2024-08-22 12:51:28 -0500

    Time Loop

---
+++ Module_3/Module-3-Labs/Loops/for_loop_types.py
@@ -0,0 +1,13 @@
+number = 12
+
+# Use the range function 
+for number in range(100):
+      print("hello world")
+      pass
+
+power = 1
+for expo in range(16):
+    print("2 to the power of", expo, "is", power)
+    power *= 2
+
+# expo variable is used as the control variable for the loop and indicates the current value of the exponent 
---
+++ Module_3/Module-3-Labs/Loops/time_counter.py
@@ -0,0 +1,8 @@
+import time
+
+number = 0 
+
+for number in range(1, 6):
+      print(number, "Mississippi")
+      number += 1
+print ("Ready or not here I come!")
